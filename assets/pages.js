210ae23cee495aeca88899c434d6c7c3e09cd262
ezermoysis <ezer.moysis14@imperial.ac.uk>
2017-05-24 17:18:05 +0100

    deploy

---
+++ assets/pages.js
@@ -0,0 +1,14 @@
+function show1(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+}
+var userb;
+
+function us(){
+  userb = 1;
+}
+
+function pr(){
+  userb = 0;
+}
